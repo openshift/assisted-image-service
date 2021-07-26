4db243f362f8d89af0977cbc4f7d67f5c2ed252b
Nick Carboni <ncarboni@redhat.com>
2021-07-26 16:38:29 -0400

    Remove iso customization from editor
    This is going to implemented using streaming so we don't need the
    file-based customization here.

--- internal/isoeditor/rhcos.go
+++ internal/isoeditor/rhcos.go
@@ -2,31 +2,17 @@ package isoeditor
 
 import (
 	"bytes"
-	"compress/gzip"
 	"encoding/binary"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"regexp"
-	"text/template"
 
-	"github.com/cavaliercoder/go-cpio"
-	"github.com/openshift/assisted-service/internal/constants"
-	"github.com/openshift/assisted-service/internal/isoutil"
-	"github.com/openshift/assisted-service/pkg/staticnetworkconfig"
+	"github.com/carbonin/assisted-image-service/internal/isoutil"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
-
-const rootfsServiceConfigFormat = `[Service]
-Environment=http_proxy={{.HTTP_PROXY}}
-Environment=https_proxy={{.HTTPS_PROXY}}
-Environment=no_proxy={{.NO_PROXY}}
-Environment=HTTP_PROXY={{.HTTP_PROXY}}
-Environment=HTTPS_PROXY={{.HTTPS_PROXY}}
-Environment=NO_PROXY={{.NO_PROXY}}`
 
 const (
 	RamDiskPaddingLength  = uint64(1024 * 1024) // 1MB
@@ -35,16 +21,7 @@ const (
 	ramDiskImagePath      = "/images/assisted_installer_custom.img"
 	ignitionHeaderKey     = "coreiso+"
 	ramdiskHeaderKey      = "ramdisk+"
-	isoSystemAreaSize     = 32768
-	ignitionHeaderSize    = 24
-	headerLength          = int64(32768) // first 32KB in ISO
 )
-
-type ClusterProxyInfo struct {
-	HTTPProxy  string
-	HTTPSProxy string
-	NoProxy    string
-}
 
 type OffsetInfo struct {
 	Key    [8]byte
@@ -55,14 +32,12 @@ type OffsetInfo struct {
 //go:generate mockgen -package=isoeditor -destination=mock_editor.go -self_package=github.com/openshift/assisted-service/internal/isoeditor . Editor
 type Editor interface {
 	CreateMinimalISOTemplate(rootFSURL string) (string, error)
-	CreateClusterMinimalISO(ignition string, staticNetworkConfig string, clusterProxyInfo *ClusterProxyInfo) (string, error)
 }
 
 type rhcosEditor struct {
-	isoHandler          isoutil.Handler
-	log                 logrus.FieldLogger
-	workDir             string
-	staticNetworkConfig staticnetworkconfig.StaticNetworkConfig
+	isoHandler isoutil.Handler
+	log        logrus.FieldLogger
+	workDir    string
 }
 
 // Creates the template minimal iso by removing the rootfs and adding the url
@@ -99,38 +74,6 @@ func (e *rhcosEditor) CreateMinimalISOTemplate(rootFSURL string) (string, error)
 	}
 
 	return isoPath, nil
-}
-
-func (e *rhcosEditor) CreateClusterMinimalISO(ignition string, staticNetworkConfig string, clusterProxyInfo *ClusterProxyInfo) (string, error) {
-	clusterISOPath, err := tempFileName(e.workDir)
-	if err != nil {
-		return "", err
-	}
-
-	if err = e.isoHandler.Copy(clusterISOPath); err != nil {
-		return "", errors.Wrap(err, "failed to copy iso")
-	}
-
-	ignitionOffsetInfo, ramDiskOffsetInfo, err := readHeader(clusterISOPath)
-	if err != nil {
-		return "", err
-	}
-
-	if err := e.addIgnitionArchive(clusterISOPath, ignition, ignitionOffsetInfo.Offset); err != nil {
-		return "", errors.Wrap(err, "failed to add ignition archive")
-	}
-
-	if staticNetworkConfig != "" || clusterProxyInfo.HTTPProxy != "" || clusterProxyInfo.HTTPSProxy != "" {
-		if err := e.addCustomRAMDisk(clusterISOPath, staticNetworkConfig, clusterProxyInfo, ramDiskOffsetInfo); err != nil {
-			return "", errors.Wrap(err, "failed to add additional ramdisk")
-		}
-	}
-
-	if err := e.isoHandler.CleanWorkDir(); err != nil {
-		e.log.WithError(err).Warnf("Failed to clean isoHandler work dir")
-	}
-
-	return clusterISOPath, nil
 }
 
 func (e *rhcosEditor) embedInitrdPlaceholders() error {
@@ -185,115 +128,6 @@ func (e *rhcosEditor) createImagePlaceholder(imagePath string, paddingLength uin
 
 	err = f.Truncate(int64(paddingLength))
 	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (e *rhcosEditor) addIgnitionArchive(clusterISOPath, ignition string, ignitionOffset uint64) error {
-	archiveBytes, err := IgnitionImageArchive(ignition)
-	if err != nil {
-		return err
-	}
-
-	return writeAt(archiveBytes, int64(ignitionOffset), clusterISOPath)
-}
-
-func (e *rhcosEditor) addCustomRAMDisk(clusterISOPath, staticNetworkConfig string, clusterProxyInfo *ClusterProxyInfo, ramdiskOffsetInfo *OffsetInfo) error {
-	buffer := new(bytes.Buffer)
-	w := cpio.NewWriter(buffer)
-	if staticNetworkConfig != "" {
-		filesList, newErr := e.staticNetworkConfig.GenerateStaticNetworkConfigData(staticNetworkConfig)
-		if newErr != nil {
-			return newErr
-		}
-		for _, file := range filesList {
-			err := addFileToArchive(w, filepath.Join("/etc/assisted/network", file.FilePath), file.FileContents, 0o600)
-			if err != nil {
-				return err
-			}
-		}
-		scriptPath := "/usr/lib/dracut/hooks/initqueue/settled/90-assisted-pre-static-network-config.sh"
-		scriptContent := constants.PreNetworkConfigScript
-
-		if err := addFileToArchive(w, scriptPath, scriptContent, 0o755); err != nil {
-			return err
-		}
-	}
-	if clusterProxyInfo.HTTPProxy != "" || clusterProxyInfo.HTTPSProxy != "" {
-		rootfsServiceConfigPath := "/etc/systemd/system/coreos-livepxe-rootfs.service.d/10-proxy.conf"
-		rootfsServiceConfig, err := e.formatRootfsServiceConfigFile(clusterProxyInfo)
-		if err != nil {
-			return err
-		}
-		if err := addFileToArchive(w, rootfsServiceConfigPath, rootfsServiceConfig, 0o664); err != nil {
-			return err
-		}
-	}
-	if err := w.Close(); err != nil {
-		return err
-	}
-
-	// Compress custom RAM disk
-	compressedArchive, err := getCompressedArchive(buffer)
-	if err != nil {
-		return err
-	}
-
-	// Ensures RAM placeholder is large enough to accommodate the compressed archive
-	if uint64(len(compressedArchive)) > ramdiskOffsetInfo.Length {
-		return errors.Errorf("Custom RAM disk is larger than the placeholder in ISO (%d bytes > %d bytes)",
-			len(compressedArchive), RamDiskPaddingLength)
-	}
-
-	return writeAt(compressedArchive, int64(ramdiskOffsetInfo.Offset), clusterISOPath)
-}
-
-func (e *rhcosEditor) formatRootfsServiceConfigFile(clusterProxyInfo *ClusterProxyInfo) (string, error) {
-	var rootfsServicConfigParams = map[string]string{
-		"HTTP_PROXY":  clusterProxyInfo.HTTPProxy,
-		"HTTPS_PROXY": clusterProxyInfo.HTTPSProxy,
-		"NO_PROXY":    clusterProxyInfo.NoProxy,
-	}
-	tmpl, err := template.New("rootfsServiceConfig").Parse(rootfsServiceConfigFormat)
-	if err != nil {
-		return "", err
-	}
-	buf := &bytes.Buffer{}
-	if err = tmpl.Execute(buf, rootfsServicConfigParams); err != nil {
-		return "", err
-	}
-	return buf.String(), nil
-}
-
-func addFileToArchive(w *cpio.Writer, path string, content string, mode cpio.FileMode) error {
-	// add all the directories in path in the correct order, using dirsStack as FILO
-	dirsStack := []string{}
-	for dir := filepath.Dir(path); dir != "" && dir != "/"; dir = filepath.Dir(dir) {
-		dirsStack = append(dirsStack, dir)
-	}
-	for i := len(dirsStack) - 1; i >= 0; i-- {
-		hdr := &cpio.Header{
-			Name: dirsStack[i],
-			Mode: 040755,
-			Size: 0,
-		}
-		if err := w.WriteHeader(hdr); err != nil {
-			return err
-		}
-	}
-
-	// add the file content
-	hdr := &cpio.Header{
-		Name: path,
-		Mode: mode,
-		Size: int64(len(content)),
-	}
-	if err := w.WriteHeader(hdr); err != nil {
-		return err
-	}
-	if _, err := w.Write([]byte(content)); err != nil {
 		return err
 	}
 
@@ -377,88 +211,6 @@ func tempFileName(baseDir string) (string, error) {
 	return path, nil
 }
 
-func writeAt(b []byte, offset int64, clusterISOPath string) error {
-	iso, err := os.OpenFile(clusterISOPath, os.O_WRONLY, 0o664)
-	if err != nil {
-		return err
-	}
-	defer iso.Close()
-
-	_, err = iso.WriteAt(b, offset)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// IgnitionImageArchive takes an ignitionConfig and returns a gzipped CPIO
-// archive (in bytes) or err on failure.
-func IgnitionImageArchive(ignitionConfig string) ([]byte, error) {
-	ignitionBytes := []byte(ignitionConfig)
-
-	// Create CPIO archive
-	archiveBuffer := new(bytes.Buffer)
-	cpioWriter := cpio.NewWriter(archiveBuffer)
-	if err := cpioWriter.WriteHeader(&cpio.Header{Name: "config.ign", Mode: 0o100_644, Size: int64(len(ignitionBytes))}); err != nil {
-		return nil, errors.Wrap(err, "Failed to write CPIO header")
-	}
-	if _, err := cpioWriter.Write(ignitionBytes); err != nil {
-
-		return nil, errors.Wrap(err, "Failed to write CPIO archive")
-	}
-	if err := cpioWriter.Close(); err != nil {
-		return nil, errors.Wrap(err, "Failed to close CPIO archive")
-	}
-
-	return getCompressedArchive(archiveBuffer)
-}
-
-func getCompressedArchive(archiveBuffer *bytes.Buffer) ([]byte, error) {
-	// Run gzip compression
-	compressedBuffer := new(bytes.Buffer)
-	gzipWriter := gzip.NewWriter(compressedBuffer)
-	if _, err := gzipWriter.Write(archiveBuffer.Bytes()); err != nil {
-		return nil, errors.Wrap(err, "Failed to gzip archive")
-	}
-	if err := gzipWriter.Close(); err != nil {
-		return nil, errors.Wrap(err, "Failed to gzip archive")
-	}
-
-	return compressedBuffer.Bytes(), nil
-}
-
-func readHeader(isoPath string) (*OffsetInfo, *OffsetInfo, error) {
-	iso, err := os.OpenFile(isoPath, os.O_RDONLY, 0o664)
-	if err != nil {
-		return nil, nil, err
-	}
-	defer iso.Close()
-
-	// Read 48 bytes with offsets metadata from the end of system area
-	ignitionMetadata := make([]byte, 24)
-	_, err = iso.ReadAt(ignitionMetadata, headerLength-24)
-	if err != nil {
-		return nil, nil, err
-	}
-	ramdiskMetadata := make([]byte, 24)
-	_, err = iso.ReadAt(ramdiskMetadata, headerLength-48)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	ignitionOffsetInfo, err := GetIgnitionArea(ignitionMetadata)
-	if err != nil {
-		return nil, nil, err
-	}
-	ramdiskOffsetInfo, err := GetRamDiskArea(ramdiskMetadata)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return ignitionOffsetInfo, ramdiskOffsetInfo, nil
-}
-
 // Writing the offsets of initrd images in the end of system area (first 32KB).
 // As the ISO template is generated by us, we know that this area should be empty.
 func writeHeader(ignitionOffsetInfo, ramDiskOffsetInfo *OffsetInfo, isoPath string) error {
@@ -506,92 +258,4 @@ func writeOffsetInfo(headerOffset int64, offsetInfo *OffsetInfo, iso *os.File) (
 	}
 
 	return bytesLength, nil
-}
-
-func validateOffsetInfoKey(offsetInfo *OffsetInfo, key string) error {
-	if string(offsetInfo.Key[:]) != key {
-		return errors.Errorf("Invalid key in offset metadata: %s", key)
-	}
-	return nil
-}
-
-func GetIgnitionArea(offsetMetadata []byte) (*OffsetInfo, error) {
-	offsetInfo, err := ParseOffsetInfo(offsetMetadata)
-	if err != nil {
-		return nil, err
-	}
-	if err = validateOffsetInfoKey(offsetInfo, ignitionHeaderKey); err != nil {
-		return nil, err
-	}
-	return offsetInfo, nil
-}
-
-func GetRamDiskArea(offsetMetadata []byte) (*OffsetInfo, error) {
-	offsetInfo, err := ParseOffsetInfo(offsetMetadata)
-	if err != nil {
-		return nil, err
-	}
-	if err = validateOffsetInfoKey(offsetInfo, ramdiskHeaderKey); err != nil {
-		return nil, err
-	}
-	return offsetInfo, nil
-}
-
-// ParseOffsetInfo gets a 24 bytes array with offset metadata and
-// returns an OffsetInfo struct with the parsed data.
-func ParseOffsetInfo(headerBytes []byte) (*OffsetInfo, error) {
-	buf := bytes.NewReader(headerBytes)
-	offsetInfo := new(OffsetInfo)
-	err := binary.Read(buf, binary.LittleEndian, offsetInfo)
-	if err != nil {
-		return nil, err
-	}
-	return offsetInfo, nil
-}
-
-func EmbedIgnition(inputISOPath, outputISOPath, ignitionConfig string) error {
-	coreosIgnitionHeader := make([]byte, ignitionHeaderSize)
-
-	inputISO, err := os.Open(inputISOPath)
-	if err != nil {
-		return err
-	}
-	defer inputISO.Close()
-
-	// Reading the last 24 bytes at the end of the system area)
-	if _, err = inputISO.ReadAt(coreosIgnitionHeader, isoSystemAreaSize-ignitionHeaderSize); err != nil {
-		return err
-	}
-	ignitionOffsetInfo, err := GetIgnitionArea(coreosIgnitionHeader)
-	if err != nil {
-		return err
-	}
-
-	cpio, err := IgnitionImageArchive(ignitionConfig)
-	if err != nil {
-		return err
-	}
-
-	if uint64(len(cpio)) > ignitionOffsetInfo.Length {
-		return errors.Errorf("Compressed Ignition config is too large: %v > %v", len(cpio), int(ignitionOffsetInfo.Length))
-	}
-
-	resultISO, err := os.Create(outputISOPath)
-	if err != nil {
-		return err
-	}
-	defer resultISO.Close()
-
-	if _, err = io.Copy(resultISO, inputISO); err != nil {
-		return err
-	}
-
-	// clear out the embed area
-	embedArea := make([]byte, ignitionOffsetInfo.Length)
-	copy(embedArea, cpio)
-	if _, err = resultISO.WriteAt(embedArea, int64(ignitionOffsetInfo.Offset)); err != nil {
-		return err
-	}
-
-	return nil
 }
--- internal/isoeditor/rhcos_test.go
+++ internal/isoeditor/rhcos_test.go
@@ -1,9 +1,9 @@
 package isoeditor
 
 import (
-	"compress/gzip"
+	"bytes"
+	"encoding/binary"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -11,13 +11,9 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/cavaliercoder/go-cpio"
-	"github.com/golang/mock/gomock"
+	"github.com/carbonin/assisted-image-service/internal/isoutil"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/openshift/assisted-service/internal/constants"
-	"github.com/openshift/assisted-service/internal/isoutil"
-	"github.com/openshift/assisted-service/pkg/staticnetworkconfig"
 	"github.com/sirupsen/logrus"
 )
 
@@ -38,13 +34,11 @@ func TestIsoEditor(t *testing.T) {
 
 var _ = Context("with test files", func() {
 	var (
-		isoDir                  string
-		isoFile                 string
-		filesDir                string
-		workDir                 string
-		volumeID                = "Assisted123"
-		ctrl                    *gomock.Controller
-		mockStaticNetworkConfig *staticnetworkconfig.MockStaticNetworkConfig
+		isoDir   string
+		isoFile  string
+		filesDir string
+		workDir  string
+		volumeID = "Assisted123"
 	)
 
 	BeforeSuite(func() {
@@ -58,8 +52,6 @@ var _ = Context("with test files", func() {
 
 	BeforeEach(func() {
 		var err error
-		ctrl = gomock.NewController(GinkgoT())
-		mockStaticNetworkConfig = staticnetworkconfig.NewMockStaticNetworkConfig(ctrl)
 		workDir, err = ioutil.TempDir("", "testisoeditor")
 		Expect(err).NotTo(HaveOccurred())
 	})
@@ -71,7 +63,7 @@ var _ = Context("with test files", func() {
 
 	Describe("CreateMinimalISOTemplate", func() {
 		It("iso created successfully", func() {
-			editor := editorForFile(isoFile, workDir, mockStaticNetworkConfig)
+			editor := editorForFile(isoFile, workDir)
 			err := editor.(*rhcosEditor).embedOffsetsInSystemArea(isoFile)
 			Expect(err).ToNot(HaveOccurred())
 			file, err := editor.CreateMinimalISOTemplate(testRootFSURL)
@@ -85,29 +77,15 @@ var _ = Context("with test files", func() {
 		})
 
 		It("missing iso file", func() {
-			editor := editorForFile("invalid", workDir, mockStaticNetworkConfig)
+			editor := editorForFile("invalid", workDir)
 			_, err := editor.CreateMinimalISOTemplate(testRootFSURL)
 			Expect(err).To(HaveOccurred())
 		})
 	})
 
-	Describe("CreateClusterMinimalISO", func() {
-		It("cluster ISO created successfully", func() {
-			editor := editorForFile(isoFile, workDir, mockStaticNetworkConfig)
-			proxyInfo := &ClusterProxyInfo{}
-			file, err := editor.CreateClusterMinimalISO("ignition", "", proxyInfo)
-			Expect(err).ToNot(HaveOccurred())
-
-			_, err = os.Stat(workDir)
-			Expect(os.IsNotExist(err)).To(BeTrue())
-
-			os.Remove(file)
-		})
-	})
-
 	Describe("fixTemplateConfigs", func() {
 		It("alters the kernel parameters correctly", func() {
-			editor := editorForFile(isoFile, workDir, mockStaticNetworkConfig)
+			editor := editorForFile(isoFile, workDir)
 			isoHandler := editor.(*rhcosEditor).isoHandler
 
 			err := isoHandler.Extract()
@@ -131,16 +109,32 @@ var _ = Context("with test files", func() {
 	})
 
 	Describe("embedOffsetsInSystemArea", func() {
+		getOffsetInfo := func(f *os.File, loc int64) *OffsetInfo {
+			meta := make([]byte, 24)
+			count, err := f.ReadAt(meta, loc)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(count).To(Equal(24))
+
+			buf := bytes.NewReader(meta)
+			info := new(OffsetInfo)
+			err = binary.Read(buf, binary.LittleEndian, info)
+			Expect(err).ToNot(HaveOccurred())
+
+			return info
+		}
+
 		It("embeds offsets in system area correctly", func() {
-			editor := editorForFile(isoFile, workDir, mockStaticNetworkConfig)
+			editor := editorForFile(isoFile, workDir)
 
 			// Create template
 			isoPath, err := editor.CreateMinimalISOTemplate(testRootFSURL)
 			Expect(err).ToNot(HaveOccurred())
 
 			// Read offsets
-			ignitionOffsetInfo, ramDiskOffsetInfo, err := readHeader(isoPath)
+			f, err := os.OpenFile(isoPath, os.O_RDONLY, 0o664)
 			Expect(err).ToNot(HaveOccurred())
+			ignitionOffsetInfo := getOffsetInfo(f, 32744)
+			ramDiskOffsetInfo := getOffsetInfo(f, 32720)
 
 			// Validate ignitionOffsetInfo
 			ignitionOffset, err := isoutil.GetFileLocation(ignitionImagePath, isoPath)
@@ -157,118 +151,12 @@ var _ = Context("with test files", func() {
 			Expect(ramDiskOffsetInfo.Length).To(Equal(RamDiskPaddingLength))
 		})
 	})
-
-	Describe("addCustomRAMDisk", func() {
-		It("adds a new archive correctly", func() {
-			editor := editorForFile(isoFile, workDir, mockStaticNetworkConfig)
-			isoHandler := editor.(*rhcosEditor).isoHandler
-			clusterProxyInfo := ClusterProxyInfo{
-				HTTPProxy:  "http://10.10.1.1:3128",
-				HTTPSProxy: "https://10.10.1.1:3128",
-				NoProxy:    "quay.io",
-			}
-
-			staticnetworkConfigOutput := []staticnetworkconfig.StaticNetworkConfigData{
-				{
-					FilePath:     "1.nmconnection",
-					FileContents: "1.nmconnection contents",
-				},
-				{
-					FilePath:     "2.nmconnection",
-					FileContents: "2.nmconnection contents",
-				},
-			}
-			mockStaticNetworkConfig.EXPECT().GenerateStaticNetworkConfigData("staticnetworkconfig").Return(staticnetworkConfigOutput, nil).Times(1)
-
-			ramDiskOffset, err := isoutil.GetFileLocation(ramDiskImagePath, isoFile)
-			Expect(err).ToNot(HaveOccurred())
-
-			ramDiskSize, err := isoutil.GetFileSize(ramDiskImagePath, isoFile)
-			Expect(err).ToNot(HaveOccurred())
-
-			err = editor.(*rhcosEditor).addCustomRAMDisk(isoFile, "staticnetworkconfig", &clusterProxyInfo,
-				&OffsetInfo{
-					Offset: ramDiskOffset,
-					Length: ramDiskSize,
-				})
-			Expect(err).ToNot(HaveOccurred())
-
-			err = isoHandler.Extract()
-			Expect(err).ToNot(HaveOccurred())
-
-			By("checking that the files are present in the archive")
-			f, err := os.Open(isoHandler.ExtractedPath("images/assisted_installer_custom.img"))
-			Expect(err).ToNot(HaveOccurred())
-
-			gzipReader, err := gzip.NewReader(f)
-			Expect(err).ToNot(HaveOccurred())
-
-			var scriptContent, rootfsServiceConfigContent string
-			r := cpio.NewReader(gzipReader)
-			for {
-				hdr, err := r.Next()
-				if err == io.EOF {
-					break
-				}
-				Expect(err).ToNot(HaveOccurred())
-				switch hdr.Name {
-				case "/etc/1.nmconnection":
-					configBytes, err := ioutil.ReadAll(r)
-					Expect(err).ToNot(HaveOccurred())
-					Expect(string(configBytes)).To(Equal("1.nmconnection contents"))
-				case "/etc/2.nmconnection":
-					configBytes, err := ioutil.ReadAll(r)
-					Expect(err).ToNot(HaveOccurred())
-					Expect(string(configBytes)).To(Equal("2.nmconnection contents"))
-				case "/usr/lib/dracut/hooks/initqueue/settled/90-assisted-pre-static-network-config.sh":
-					scriptBytes, err := ioutil.ReadAll(r)
-					Expect(err).ToNot(HaveOccurred())
-					scriptContent = string(scriptBytes)
-				case "/etc/systemd/system/coreos-livepxe-rootfs.service.d/10-proxy.conf":
-					rootfsServiceConfigBytes, err := ioutil.ReadAll(r)
-					Expect(err).ToNot(HaveOccurred())
-					rootfsServiceConfigContent = string(rootfsServiceConfigBytes)
-				}
-			}
-
-			Expect(scriptContent).To(Equal(constants.PreNetworkConfigScript))
-
-			rootfsServiceConfig := fmt.Sprintf("[Service]\n"+
-				"Environment=http_proxy=%s\nEnvironment=https_proxy=%s\nEnvironment=no_proxy=%s\n"+
-				"Environment=HTTP_PROXY=%s\nEnvironment=HTTPS_PROXY=%s\nEnvironment=NO_PROXY=%s",
-				clusterProxyInfo.HTTPProxy, clusterProxyInfo.HTTPSProxy, clusterProxyInfo.NoProxy,
-				clusterProxyInfo.HTTPProxy, clusterProxyInfo.HTTPSProxy, clusterProxyInfo.NoProxy)
-			Expect(rootfsServiceConfigContent).To(Equal(rootfsServiceConfig))
-		})
-	})
-	It("custom RAM disk is larger than placeholder", func() {
-		editor := editorForFile(isoFile, workDir, mockStaticNetworkConfig)
-		staticNetworkConfigOutput := []staticnetworkconfig.StaticNetworkConfigData{
-			{
-				FilePath:     "1.nmconnection",
-				FileContents: "1.nmconnection contents",
-			},
-		}
-		mockStaticNetworkConfig.EXPECT().GenerateStaticNetworkConfigData("staticnetworkconfig").Return(staticNetworkConfigOutput, nil).Times(1)
-
-		ramDiskOffset, err := isoutil.GetFileLocation(ramDiskImagePath, isoFile)
-		Expect(err).ToNot(HaveOccurred())
-
-		err = editor.(*rhcosEditor).addCustomRAMDisk(isoFile, "staticnetworkconfig", &ClusterProxyInfo{},
-			&OffsetInfo{
-				Offset: ramDiskOffset,
-				Length: 10, // Set a tiny value as the archive is compressed
-			})
-		Expect(err).To(HaveOccurred())
-		Expect(err.Error()).Should(ContainSubstring("Custom RAM disk is larger than the placeholder in ISO"))
-	})
 })
 
-func editorForFile(iso string, workDir string, staticNetworkConfig staticnetworkconfig.StaticNetworkConfig) Editor {
+func editorForFile(iso string, workDir string) Editor {
 	return &rhcosEditor{
-		isoHandler:          isoutil.NewHandler(iso, workDir),
-		log:                 getTestLog(),
-		staticNetworkConfig: staticNetworkConfig,
+		isoHandler: isoutil.NewHandler(iso, workDir),
+		log:        getTestLog(),
 	}
 }
 
