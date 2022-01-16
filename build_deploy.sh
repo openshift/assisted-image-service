f4fac7af05fc345e37dd137632efe5d43628ac6b
Osher De Paz <osherdepaz@gmail.com>
2022-01-16 17:37:29 +0200

    MGMT-8927: build image from scratch to handle possible cleanups

--- build_deploy.sh
+++ build_deploy.sh
@@ -6,7 +6,7 @@ TAG=$(git rev-parse --short=7 HEAD)
 REPO="quay.io/app-sre/assisted-image-service"
 IMAGE="${REPO}:${TAG}"
 
-docker build -f Dockerfile.image-service . -t ${IMAGE}
+docker build -f Dockerfile.image-service . -t ${IMAGE} --no-cache
 
 docker login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 
