eabee1ab6d558f99ce52af2e7a0e01bd6d4ea87d
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-05-02 17:50:16 +0200

    Bump golangci-lint to 1.45.2

--- hack/setup_env.sh
+++ hack/setup_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -eux
 
 yum install -y install podman genisoimage && yum clean all
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.25.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.45.2
 GOFLAGS='' go install golang.org/x/tools/cmd/goimports@v0.1.5
 GOFLAGS='' go install github.com/golang/mock/mockgen@v1.6.0
