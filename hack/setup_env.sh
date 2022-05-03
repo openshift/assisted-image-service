0134655ff44e1e7efc18ec258b04a8969155ad3b
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-05-03 08:07:25 +0200

    hack: use `go install` to install goimports and mockgen
    `go get` mutates go.mod/go.sum, so we should use "go install" to install additional tools

--- hack/setup_env.sh
+++ hack/setup_env.sh
@@ -2,4 +2,5 @@
 
 yum install -y install podman genisoimage && yum clean all
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.25.0
-go get -u golang.org/x/tools/cmd/goimports@v0.1.5 github.com/golang/mock/mockgen@v1.6.0
+GOFLAGS='' go install golang.org/x/tools/cmd/goimports@v0.1.5
+GOFLAGS='' go install github.com/golang/mock/mockgen@v1.6.0
