8a156ec1aeb716da15b441e94f1e6fbef915b662
Jacob Emery <jacob.emery@ibm.com>
2022-12-19 14:02:10 -0600

    Skip minimal iso extraction if arch is s390x
    There is no way for the image service to edit the kernel parameters for
    the s390x ISO which means the rootfs url parameter can't be added. This
    makes it impossible to create a minimal ISO template.
    
    https://issues.redhat.com/browse/COS-1924 was opened to address this
    issue

--- pkg/imagestore/imagestore.go
+++ pkg/imagestore/imagestore.go
@@ -226,6 +226,11 @@ func (s *rhcosStore) Populate(ctx context.Context) error {
 		imageVersion := imageInfo["version"]
 		arch := imageInfo["cpu_architecture"]
 
+		// Don't attempt to create a minimal ISO for s390x because there's no easy way to edit the kernel parameters
+		// This means that the rootfs URL can't be added which makes it impossible for us to create a minimal ISO
+		if arch == "s390x" {
+			continue
+		}
 		minimalPath := filepath.Join(s.dataDir, isoFileName(ImageTypeMinimal, openshiftVersion, imageVersion, arch))
 		if _, err := os.Stat(minimalPath); os.IsNotExist(err) {
 			log.Infof("Creating minimal iso for %s-%s-%s", openshiftVersion, imageVersion, arch)
