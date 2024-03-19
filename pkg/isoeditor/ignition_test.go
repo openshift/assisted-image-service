39bd9999768d3a3341915e0b5a135f12c8c4ed79
Zane Bitter <zbitter@redhat.com>
2024-03-19 10:55:27 -0400

    Pad ignition archive to align to word boundary (#186)
    In the Linux initramfs buffer format, CPIO archives must be aligned on
    (4-byte) word boundaries. Although gzipped archives have no such
    requirement, nor is there any guarantees that the end of the archive
    will fall on a word boundary. To ensure that an uncompressed CPIO
    archive can always be appended to the initramfs, pad the end of the
    gzipped archive with null bytes to reach a word boundary. The buffer
    format always allows an arbitrary number of null bytes between archives.
    
    See https://www.kernel.org/doc/html//v5.6/driver-api/early-userspace/buffer-format.html
    for the specification.

--- pkg/isoeditor/ignition_test.go
+++ pkg/isoeditor/ignition_test.go
@@ -17,7 +17,7 @@ var _ = Describe("IgnitionContent.Archive", func() {
 			3, 3, 3, 67, 113, 126, 110, 106, 102, 122, 94, 102, 73, 102, 126, 94,
 			114, 126, 94, 73, 106, 94, 9, 3, 138, 123, 8, 1, 98, 213, 225, 116,
 			79, 72, 144, 163, 167, 143, 107, 144, 162, 162, 34, 200, 61, 128, 0, 0,
-			0, 255, 255, 191, 236, 44, 242, 12, 1, 0, 0}
+			0, 255, 255, 191, 236, 44, 242, 12, 1, 0, 0, 0}
 	)
 
 	It("converts the ignition to a compressed CPIO archive", func() {
@@ -29,5 +29,6 @@ var _ = Describe("IgnitionContent.Archive", func() {
 		ignitionBytes, err := io.ReadAll(data)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(ignitionBytes).To(Equal(ignitionArchiveBytes))
+		Expect(len(ignitionBytes) % 4).To(Equal(0))
 	})
 })
