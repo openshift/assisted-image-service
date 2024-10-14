e34e1050289b6cd9a6e1f999739948dff47064df
Gilad Ravid <60739156+giladravid16@users.noreply.github.com>
2024-10-14 16:10:58 +0300

    MGMT-19128: Migrate assisted-image-service to Konflux (#281)

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,14 +2,17 @@
 
 set -exu
 
-TAG=$(git rev-parse --short=7 HEAD)
+TAG=$(git rev-parse HEAD)
+SHORT_TAG=$(git rev-parse --short=7 HEAD)
 REPO="quay.io/app-sre/assisted-image-service"
 IMAGE="${REPO}:${TAG}"
 
-docker build -f Dockerfile.image-service . -t ${IMAGE} --no-cache
+docker build -f Dockerfile.image-service . -t "${IMAGE}" --no-cache
 
 docker login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 
+docker tag "${IMAGE}" "${REPO}:${SHORT_TAG}"
 docker tag "${IMAGE}" "${REPO}:latest"
 docker push "${IMAGE}"
+docker push "${REPO}:${SHORT_TAG}"
 docker push "${REPO}:latest"
