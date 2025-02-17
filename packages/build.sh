fa9c3dced6e1337028f456042579d2078ac22419
Juan Hernandez <juan.hernandez@redhat.com>
2025-02-17 10:00:27 +0100

    NO-ISSUE: Add `dump.erofs` to the image
    This patch adds the `dump.erofs` binary to the image. To do so it
    fetches the source and builds it inside a separate _tools_ container.
    
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ packages/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+# We will be installing tools to the '/usr/local' directories, so we need to configure the package
+# search path and generate the binaries so that they will look for dynamic libraries there.
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
+export LDFLAGS="-Wl,-rpath=/usr/local/lib"
+
+# Build the package, in the right order to account for build dependencies:
+(cd xz && ./build.sh)
+(cd erofs-utils && ./build.sh)
