81f77275e72cbdcac9e29670513c94be468fb4a9
Linoy Hadad <lhadad@redhat.com>
2025-08-18 20:12:48 +0300

    MGMT-21276: Add nmstatectl binary for the s390x arch (#462)
    The nmstatectl binary is never fetched for the s390x architecture.
    This fix addresses the issue by decoupling the extraction of nmstatectl from the minimal ISO flow - since minimal ISO is not supported for s390x. Instead, the binary is now cached, ensuring it is available for s390x as well.

--- integration_test/images_test.go
+++ integration_test/images_test.go
@@ -19,8 +19,10 @@ import (
 
 	"github.com/cavaliercoder/go-cpio"
 	diskfs "github.com/diskfs/go-diskfs"
+	"github.com/diskfs/go-diskfs/filesystem"
 	"github.com/google/uuid"
 	"github.com/onsi/gomega/ghttp"
+	"github.com/openshift/assisted-image-service/internal/common"
 	"github.com/openshift/assisted-image-service/internal/handlers"
 	"github.com/openshift/assisted-image-service/pkg/imagestore"
 	"github.com/openshift/assisted-image-service/pkg/isoeditor"
@@ -76,6 +78,18 @@ var (
 			"cpu_architecture":  "x86_64",
 			"url":               "https://okd-scos.s3.amazonaws.com/okd-scos/builds/413.9.202302280609-0/x86_64/scos-413.9.202302280609-0-live.x86_64.iso",
 			"version":           "x86_64-latest",
+		},
+		{
+			"openshift_version": "4.18",
+			"cpu_architecture":  "s390x",
+			"url":               "https://mirror.openshift.com/pub/openshift-v4/s390x/dependencies/rhcos/4.18/4.18.1/rhcos-4.18.1-s390x-live.s390x.iso",
+			"version":           "s390x-418",
+		},
+		{
+			"openshift_version": "4.18",
+			"cpu_architecture":  "x86_64",
+			"url":               "https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/4.18/4.18.1/rhcos-4.18.1-x86_64-live.x86_64.iso",
+			"version":           "x86_64-418",
 		},
 	}
 
@@ -235,20 +249,15 @@ var _ = Describe("Image integration tests", func() {
 					Expect(err).NotTo(HaveOccurred())
 
 					By("verifying ignition content")
-					f, err := fs.OpenFile("/images/ignition.img", os.O_RDONLY)
+					rc, err := ignitionPayloadReader(fs, version)
 					Expect(err).NotTo(HaveOccurred())
-					gzipReader, err := gzip.NewReader(f)
-					Expect(err).NotTo(HaveOccurred())
-					cpioReader := cpio.NewReader(gzipReader)
-					hdr, err := cpioReader.Next()
-					Expect(err).NotTo(HaveOccurred())
-					Expect(hdr.Name).To(Equal("config.ign"))
-					Expect(hdr.Size).To(Equal(int64(len(tc.expectedIgnition))))
-					content, err := io.ReadAll(cpioReader)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(content).To(Equal(tc.expectedIgnition))
+					defer rc.Close()
 
-					if tc.expectedRamdisk != nil {
+					got, err := readIgnitionContentFromGzCpio(rc)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(got).To(Equal(tc.expectedIgnition))
+
+					if len(tc.expectedRamdisk) > 0 {
 						By("verifying ramdisk content")
 						f, err := fs.OpenFile("/images/assisted_installer_custom.img", os.O_RDONLY)
 						Expect(err).NotTo(HaveOccurred())
@@ -272,6 +281,82 @@ var _ = Describe("Image integration tests", func() {
 				})
 			}
 		})
+		Context("nmstate archive verification - "+tc.name, func() {
+			BeforeEach(func() {
+				imageID = uuid.New().String()
+
+				// Set up assisted service
+				assistedServer = ghttp.NewServer()
+				u, err := url.Parse(assistedServer.URL())
+				Expect(err).NotTo(HaveOccurred())
+
+				// pxe-initrd path: ignition without discovery_iso_type
+				assistedServer.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", fmt.Sprintf("/api/assisted-install/v2/infra-envs/%s/downloads/files", imageID), "file_name=discovery.ign"),
+						ghttp.RespondWith(http.StatusOK, tc.expectedIgnition),
+					),
+				)
+				// pxe-initrd path: minimal-initrd always requested; 200 when provided, else 204
+				if len(tc.expectedRamdisk) > 0 {
+					assistedServer.AppendHandlers(
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", fmt.Sprintf("/api/assisted-install/v2/infra-envs/%s/downloads/minimal-initrd", imageID)),
+							ghttp.RespondWith(http.StatusOK, tc.expectedRamdisk),
+						),
+					)
+				} else {
+					assistedServer.AppendHandlers(
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", fmt.Sprintf("/api/assisted-install/v2/infra-envs/%s/downloads/minimal-initrd", imageID)),
+							ghttp.RespondWith(http.StatusNoContent, nil),
+						),
+					)
+				}
+
+				asc, err := handlers.NewAssistedServiceClient(u.Scheme, u.Host, "")
+				Expect(err).NotTo(HaveOccurred())
+
+				mdw := middleware.New(middleware.Config{})
+				imageServer = httptest.NewServer(handlers.NewImageHandler(imageStore, asc, 1, mdw))
+				imageClient = imageServer.Client()
+			})
+			AfterEach(func() {
+				assistedServer.Close()
+				imageServer.Close()
+			})
+
+			for i := range versions {
+				version := versions[i]
+
+				It("includes nmstate for "+version["openshift_version"]+" "+version["cpu_architecture"], func() {
+					ok, err := common.VersionGreaterOrEqual(version["openshift_version"], isoeditor.MinimalVersionForNmstatectl)
+					Expect(err).NotTo(HaveOccurred())
+
+					if len(tc.expectedRamdisk) <= 0 || !ok {
+						Skip(fmt.Sprintf("skipping test %s due to ocp version < 4.18 or ramdisk isn't present", tc.name))
+					}
+
+					path := fmt.Sprintf("/images/%s/pxe-initrd?version=%s&arch=%s",
+						imageID, version["openshift_version"], version["cpu_architecture"])
+					resp2, err := imageClient.Get(imageServer.URL + path)
+					Expect(err).NotTo(HaveOccurred())
+					defer resp2.Body.Close()
+					Expect(resp2.StatusCode).To(Equal(http.StatusOK))
+
+					initrdBytes, err := io.ReadAll(resp2.Body)
+					Expect(err).NotTo(HaveOccurred())
+
+					nmPath, err := imageStore.NmstatectlPathForParams(version["openshift_version"], version["cpu_architecture"])
+					Expect(err).NotTo(HaveOccurred())
+					nmBytes, err := os.ReadFile(nmPath)
+					Expect(err).NotTo(HaveOccurred())
+
+					// nmstate archive is appended last when ramdisk is present
+					Expect(bytes.HasSuffix(initrdBytes, nmBytes)).To(BeTrue(), "initrd should end with nmstate archive")
+				})
+			}
+		})
 	}
 })
 
@@ -281,7 +366,8 @@ var _ = BeforeSuite(func() {
 	imageDir, err = os.MkdirTemp("", "imagesTest")
 	Expect(err).To(BeNil())
 
-	imageStore, err = imagestore.NewImageStore(isoeditor.NewEditor(imageDir, isoeditor.NewNmstateHandler(imageDir, &isoeditor.CommonExecuter{})), imageDir, imageServiceBaseURL, false, versions, "", map[string]string{}, map[string]string{})
+	nmstateHandler := isoeditor.NewNmstateHandler(imageDir, &isoeditor.CommonExecuter{})
+	imageStore, err = imagestore.NewImageStore(isoeditor.NewEditor(imageDir, nmstateHandler), imageDir, imageServiceBaseURL, false, versions, "", map[string]string{}, map[string]string{}, nmstateHandler)
 	Expect(err).NotTo(HaveOccurred())
 
 	err = imageStore.Populate(context.Background())
@@ -299,4 +385,71 @@ func TestIntegration(t *testing.T) {
 	}
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "image building tests")
+}
+
+func ignitionPayloadReader(fs filesystem.FileSystem, version map[string]string) (io.ReadCloser, error) {
+	ignInfoFile, err := fs.OpenFile("/coreos/igninfo.json", os.O_RDONLY)
+	if err == nil {
+		defer ignInfoFile.Close()
+
+		var ignInfoBytes []byte
+		ignInfoBytes, err = io.ReadAll(ignInfoFile)
+		if err != nil {
+			return nil, err
+		}
+		var ignInfo struct {
+			File   string `json:"file,omitempty"`
+			Length int64  `json:"length,omitempty"`
+			Offset int64  `json:"offset,omitempty"`
+		}
+		if err = json.Unmarshal(ignInfoBytes, &ignInfo); err != nil {
+			return nil, err
+		}
+
+		var containerFile filesystem.File
+		containerFile, err = fs.OpenFile(ignInfo.File, os.O_RDONLY)
+		if err != nil {
+			return nil, err
+		}
+		defer containerFile.Close()
+
+		var containerBytes []byte
+		containerBytes, err = io.ReadAll(containerFile)
+		if err != nil {
+			return nil, err
+		}
+		end := ignInfo.Offset + ignInfo.Length
+		if ignInfo.Length == 0 || end > int64(len(containerBytes)) {
+			end = int64(len(containerBytes))
+		}
+		ignitionBytes := bytes.TrimRight(containerBytes[ignInfo.Offset:end], "\x00")
+
+		return io.NopCloser(bytes.NewReader(ignitionBytes)), nil
+	}
+
+	// fallback to the default path: ignition.img
+	var f filesystem.File
+	f, err = fs.OpenFile("/images/ignition.img", os.O_RDONLY)
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
+}
+
+func readIgnitionContentFromGzCpio(r io.Reader) ([]byte, error) {
+	gr, err := gzip.NewReader(r)
+	if err != nil {
+		return nil, err
+	}
+	defer gr.Close()
+
+	cr := cpio.NewReader(gr)
+	hdr, err := cr.Next()
+	if err != nil {
+		return nil, err
+	}
+	if hdr.Name != "config.ign" {
+		return nil, fmt.Errorf("unexpected cpio entry: %s", hdr.Name)
+	}
+	return io.ReadAll(cr)
 }
